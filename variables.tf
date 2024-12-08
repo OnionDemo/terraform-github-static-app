daf41687c79d44cef536451ef6ec6a602331b547
Ting <ting@hashicorp.com>
2024-12-07 17:51:27 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "template_org" {
   description = "The name of the organization in Github that will contain the example app repo."
-  default     = "hashicorp-education"
+  default ="OnionDemo"
+  //default     = "hashicorp-education"
 }
 
 variable "template_repo" {
@@ -10,7 +11,8 @@ variable "template_repo" {
 
 variable "destination_org" {
   description = "The name of the organization in Github that will contain the templated repo."
-  default     = "hashicorp-education"
+  default ="OnionDemo"
+  //default     = "hashicorp-education"
 }
 
 variable "gh_token" {
